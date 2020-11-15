b22e13435a3463c21269c3faed2f82fe5ed533c9
Sengoh <sihognes@gmail.com>
2020-11-15 14:07:30 +0100

    Created login component #2
    Missing routing to the main page

--- NTNUReads/App.tsx
+++ NTNUReads/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginForm from "./components/user/LoginForm";
+import Detailed from "./pages/Detailed";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Text>Dette funker</Text>
+      <Detailed />
     </View>
   );
 }
---
+++ NTNUReads/asyncStorage.tsx
@@ -0,0 +1,33 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const saveUser = async (value:JSON) => {
+    try {
+        await AsyncStorage.setItem(
+            '@user',
+            JSON.stringify(value)
+        )
+
+    } catch (e) {
+
+    }
+}
+export const getUser = async () => {
+    try {
+        const jsonValue = await AsyncStorage.getItem('@user')
+        console.log(jsonValue)
+        return jsonValue// != null ? JSON.parse(jsonValue) : null;
+    } catch(e) {
+        return null
+        // error reading value
+    }
+}
+
+export const removeUser = async () => {
+    try {
+        await AsyncStorage.removeItem('@user')
+    } catch(e) {
+        // remove error
+    }
+}
+
+
