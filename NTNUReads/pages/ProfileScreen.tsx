3f1a22291870187367241f54ebfaf0c7f3952e8a
Emma Valen Rian <emmavr@stud.ntnu.no>
2020-11-17 10:40:38 +0100

    #8 Added scrollview for results-table

--- NTNUReads/pages/ProfileScreen.tsx
+++ NTNUReads/pages/ProfileScreen.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {Button, StyleSheet, Text, View} from "react-native";
+import {Button, SafeAreaView, ScrollView, StyleSheet, Text} from "react-native";
 
 import { StackScreenProps } from '@react-navigation/stack';
 import {getUser, removeUser} from "../asyncStorage";
 import {NavigationProp, useNavigation} from "@react-navigation/native";
-import {DataTable} from "react-native-paper";
+import {DataTable, Title} from "react-native-paper";
+import Constants from "expo-constants";
+import url from "../url";
 
 type ProfileParamList = {
     Details: {id: number};
@@ -17,9 +19,17 @@ type ProfileProps = StackScreenProps<ProfileParamList, 'Details'>;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: "row",
         alignItems: 'center',
-        justifyContent: 'center',
+        //justifyContent: 'space-between',
+        //paddingTop: 40,
+        //padding: 10,
+        margin: 20,
+        marginTop: Constants.statusBarHeight,
     },
+    scrollView: {
+        marginHorizontal: 10,
+    }
 });
 
 
@@ -38,7 +48,7 @@ function ProfileScreen({navigation}: ProfileProps) {
         getUser().then(res => {
             if(res != null) {
                 const us = JSON.parse(res)
-                fetch("http://localhost:4000/favorite/user/"+us._id+"")
+                fetch("http://"+url+":4000/favorite/user/"+us._id+"")
                 .then(response => response.json())
                 .then((data) => {
                 //console.log(data.books);
@@ -66,36 +76,38 @@ function ProfileScreen({navigation}: ProfileProps) {
     }
 
     return (
-        <View style={styles.container}>
-            <Text>Profile Screen</Text>
-            <DataTable>
-                <DataTable.Header>
-                    <DataTable.Title>ISBN</DataTable.Title>
-                    <DataTable.Title>Forfatter</DataTable.Title>
-                    <DataTable.Title>Tittel</DataTable.Title>
-                    <DataTable.Title numeric>Vurdering</DataTable.Title>
-                </DataTable.Header>
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollView}>
+                <Title>Din profil</Title>
+                <DataTable>
+                    <DataTable.Header>
+                        <DataTable.Title>ISBN</DataTable.Title>
+                        <DataTable.Title>Forfatter</DataTable.Title>
+                        <DataTable.Title>Tittel</DataTable.Title>
+                        <DataTable.Title numeric>Vurdering</DataTable.Title>
+                    </DataTable.Header>
 
-                {searchResult.map(item =>
-                    <DataTable.Row onPress={() => navigation.push("Details", {id: item.isbn})} key={item.isbn}>
-                        <DataTable.Title>{item.isbn}</DataTable.Title>
-                        <DataTable.Title>{item.authors}</DataTable.Title>
-                        <DataTable.Title>{item.title}</DataTable.Title>
-                        <DataTable.Title numeric>{item.average_rating}</DataTable.Title>
-                    </DataTable.Row>
-                )}
-            </DataTable>
-            {/*<LoginForm/>*/}
-            <Button
-                title="Logg ut"
-                onPress={handleLogOut}
-            />
-            {/*Placeholder button with placeholder ISBN - should have same table as results here - only with favorites*/}
-            <Button
-                title="Go to DetailsScreen"
-                onPress={() => navigation.push("Details", {id: 1111111111})}
-            />
-        </View>
+                    {searchResult.map(item =>
+                        <DataTable.Row onPress={() => navigation.push("Details", {id: item.isbn})} key={item.isbn}>
+                            <DataTable.Title>{item.isbn}</DataTable.Title>
+                            <DataTable.Title>{item.authors}</DataTable.Title>
+                            <DataTable.Title>{item.title}</DataTable.Title>
+                            <DataTable.Title numeric>{item.average_rating}</DataTable.Title>
+                        </DataTable.Row>
+                    )}
+                </DataTable>
+                {/*<LoginForm/>*/}
+                <Button
+                    title="Logg ut"
+                    onPress={handleLogOut}
+                />
+                {/*Placeholder button with placeholder ISBN - should have same table as results here - only with favorites*/}
+                <Button
+                    title="Go to DetailsScreen"
+                    onPress={() => navigation.push("Details", {id: 1111111111})}
+                />
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
